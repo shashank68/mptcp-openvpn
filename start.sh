501f4b66b83054bd80e37348004757625d96c971
Shashank D <shashankindiamanoj@gmail.com>
2022-06-05 00:57:28 +0530

    Openvpn: Change to tls auth

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@ alias netex="ip netns exec"
 
 ./setup_topology.sh
 
-
+# sleep 5
 netex h2 openvpn --daemon --config ./openvpn_conf/server.conf
-
+sleep 1
 netex h1 openvpn --daemon --config ./openvpn_conf/vpn1.conf
+sleep 1
 netex h1 openvpn --daemon --config ./openvpn_conf/vpn2.conf
+sleep 1
 
 
 ip -n h1 rule add pref 10 from 11.0.0.2 table isp1
@@ -26,21 +28,17 @@ ip -n h2 mptcp limits set subflow 2 add_addr_accepted 2
 ip -n h1 mptcp endpoint flush
 ip -n h1 mptcp limits set subflow 2 add_addr_accepted 2
 ip -n h1 mptcp endpoint show
+ip -n h1 route show
 ip -n h1 mptcp endpoint add 10.0.0.12 dev tun2 id 1 subflow
 
 ip -n h1 rule add pref 10 from 10.0.0.11 table vpn1
-ip -n h1 route add default via 10.0.0.1 dev tun1 table vpn1
+ip -n h1 route add default dev tun1 table vpn1
 
 ip -n h1 rule add pref 10 from 10.0.0.12 table vpn2
-ip -n h1 route add default via 10.0.0.1 dev tun2 table vpn2
+ip -n h1 route add default  dev tun2 table vpn2
 
-
-
-echo "Ensure that the below entries are present in /etc/iproute2/rt_tables:
+echo "Ensure that the below entries are present in /etc/iproute2/rt_tables 
 101 isp1
 102 isp2
 11 vpn1
-12 vpn2
-"
-
-
+12 vpn2"
